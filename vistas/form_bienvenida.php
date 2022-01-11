2d566a857b3a6b499947ec8de8364d5668d9452d
JhoselinTumiri <74874391+JhoselinTumiri@users.noreply.github.com>
2022-01-11 10:16:14 +0100

    "asemejando a prototipo,diseño modificado"

--- vistas/form_bienvenida.php
+++ vistas/form_bienvenida.php
@@ -2,58 +2,69 @@
 include "cabecera.php";
 ?>
 <form id="form" action="index.php" method="post">
-
+    
     <div>
+     <center><h2>Inscripciones Club <br>Navarra Voleibol</h2></center>
         <div class='uno'>
             <label>Nombre</label>
             <input type="text" name="nombre" />
             <label>Apellido 1 </label>
             <input type="text" name="apellido1" />
+            
             <label>Apellido 2 </label>
             <input type="text" name="apellido2" />
             <input type="checkbox" name="sin2AP" value="no2ap" />Sin 2º Apellido<br />
-            DNI/NIE<select name="documento" holder="Seleccionar tipo de Documento">
+            <div class="datos">
+            DNI/NIE<select class= "dniCasilla" name="documento" holder="Seleccionar tipo de Documento">
                  <option value="dni" selected>Seleccionar Tipo de Documento</option>    
                 <option value="dni">DNI</option>
                 <option value="nif" >NIF</option>
                 <option value="nie">NIE</option>
             </select>
+            <input type="text" name="dni" /><br /><br>
+
             <label>telf</label>
-            <input type="tel" name="telf" /><br />
+            <input type="tel" name="telf" />
             <label>Email</label>
             <input type="email" name="email" /><br />
+            </div>
+        </div>
             <div>
                 <div class='uno'>
+                    <section>
                     <label>Sexo</label><br />
-                    <input type="radio" name="sexo" value="hombre" />Hombre<br />
+                    <input type="radio" name="sexo" value="hombre" />Hombre
                     <input type="radio" name="sexo" value="mujer" />Mujer<br />
-                    
+                    </section>
+                    <section>
                     <label>&nbsp;</label>
+                    <label>Fecha/Nac </label>
+                    <input type="date" name="fechaN" required/>
+                    </section>
                    
                 </div>
-                <label>Fecha/Nac </label>
-                <input type="date" name="fechaN" />
-                <div class='uno'>
-                    <label>Categoria</label><br />
-                    <input type="radio" name="categoria" value="cadete" />Cadete<br />
-                    <input type="radio" name="categoria" value="juvenil" />Juvenil<br />
+                
+                <div class='categoria'>
+                    <h4>Categoria</h4><br />
+                    <input type="radio" name="categoria" value="cadete" />Cadete
+                    <input type="radio" name="categoria" value="juvenil" />Juvenil
                     <input type="radio" name="categoria" value="mayores" />Mayores<br />
-                    
-                    <label>&nbsp;</label>
+                     <label>&nbsp;</label>
                    
                 </div>
                 <div class='uno'>
                     <label>Presenta Lesiones </label><br />
-                    <input type="checkbox" name="lesiones" value="rodilla" />rodilla<br />
-                    <input type="checkbox" name="lesiones" value="hombro" />hombro<br />
-                    <input type="checkbox" name="lesiones" value="tobillo" />tobillo<br />
-                    <input type="checkbox" name="lesiones" value="dedos" />dedos<br />
+                    <input type="checkbox" name="lesiones[]" value="rodilla" />rodilla<br />
+                    <input type="checkbox" name="lesiones[]" value="hombro" />hombro<br />
+                    <input type="checkbox" name="lesiones[]" value="tobillo" />tobillo<br />
+                    <input type="checkbox" name="lesiones[]" value="dedos" />dedos<br />
                     
                     <label>&nbsp;</label>
                    
                 </div>
-            </div>
-
+                </div>
+                <input class="priv" type="checkbox" name="privacidad" value="privacidad" />Acepto las condiciones de Política de Privacidad ..<br />
+           
             <input type="submit" name="enviar" value="Inscribirse" />
 
 </form>
