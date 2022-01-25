013cf7fff3d8855c87c2ce9336b7af3e2960975b
JhoselinTumiri <74874391+JhoselinTumiri@users.noreply.github.com>
2022-01-25 09:59:04 +0100

    "Añadido de reglas de Validación"

--- controladores/Controlador.php
+++ controladores/Controlador.php
@@ -1,4 +1,5 @@
 <?php
+include "includes/funciones.php";
 class Controlador
 {
     public function run()
@@ -6,7 +7,7 @@ class Controlador
         if (!isset($_POST['enviar']))//no se ha enviado el formulario
         { // primera petición
             //se llama al método para mostrar el formulario inicial
-            $this->mostrarFormulario();
+            $this->mostrarFormulario(null);
 			exit();
         } else
         {
@@ -34,10 +35,25 @@ class Controlador
                 }
 
             //Mostrar tipo de documento elegido
-            if (!empty($_POST['dni'])){
-                $dni = $_POST['dni'];
-                $resultado .= "</br>DNI/NIF = $dni <br />";
+            if (!empty($_POST['documento'])){
+                $documento = $_POST['documento'];
+                $tipo="";
+                switch($documento){
+                    CASE "dni": $tipo= "dni";
+                    break;
+                    CASE "nif": $tipo = "nif";
+                    break;
                 }
+                if($tipo == "dni"){
+                  if( !validarDni("dni")){
+                       $error="Introduzca un dni correcto";
+                    }
+                }
+               
+                 
+
+                $resultado .= "</br>DNI/NIF = $tipo <br />";
+                }  
             
             //Mostrar teléfono
             if (!empty($_POST['telf'])){
@@ -80,8 +96,9 @@ class Controlador
                         break;
                 }
             }
-            
-            //fecha de Nacimiento
+            /*
+             * Comprobar fecha de Nacimiento(obligatorio en html por defecto)
+            */
             if(isset($_POST['fechaN'])){
                 $fechaN= $_POST['fechaN'];
                 $resultado .= "<br>fecha de Nacimiento: $fechaN";
@@ -105,18 +122,51 @@ class Controlador
             }
              
             $resultado .= "<br />";
-            $this->mostrarResultado($resultado);
+            $this->mostrarFormulario($resultado);
 			exit();		
         }
     }
-    private function mostrarFormulario()
+
+    /**
+     * Creamos un método para crear un array con  las reglas de validación que sean necesarias
+     * las reglas de validación contirnrn restricciones pattern, required, maxLength, datosValidos 
+     * tipoDocumentoValido(verificará si el documento dependiendo el que sea es válido con un pattern)
+     * 
+     */
+
+     public function crearReglasDeValidacion(){
+         $reglasValidacion = array(
+             "nombre"=>array("maxLength"=> 30, "required"=>true),
+             "apellido1"=>array("maxLength"=> 20, "required"=>true),
+             "apellido2"=>array("maxLength"=> 20, "required"=>false),
+             "documento"=>array("tipoDocumentoValido"=>true, "required"=>true),
+             "telf"=>array("pattern" => "/(\+34|0034|34)?[ -]*(6|7)[ -]*([0-9][ -]*){8}/","required"=>true),//"/^(6|9)[ -]*([0-9][ -]*){8}/", "maxlength" => 9,
+             "email"=>array("filtradoPHP"=> true, "required"=>true),//"/[a-zA-Z0-9!#$%&'*_+-]([\.]?[a-zA-Z0-9!#$%&'*_+-])+@[a-zA-Z0-9]([^@&%$\/()=?¿!.,:;]|\d)+[a-zA-Z0-9][\.][a-zA-Z]{2,4}([\.][a-zA-Z]{2})?/"
+             "sexo"=>array("datosValidos"=>array("mujer", "hombre"),"required"=>true),
+             "fechaN"=>array("pattern"=> "/^(0[1-9]|[1-2]\d|3[01])(\/)(0[1-9]|1[012])\2(\d{4})$/","required"=>true),
+             "categoria"=>array("datosValidos"=>array("cadete", "juvenil", "mayores"),"required"=>true),
+             "lesiones[]"=>array("datosValidos"=>array("rodilla", "hombro", "tobillo", "dedos"),"required"=>false)
+         );
+     }
+
+    /**
+     * Método validar 
+     * Realiza el proceso de validacion utilizando un Objeto de clase y el método reglasDeValidacion, creada anteriormente 
+     */
+    public function validar(){
+        $validador = new ValidadorForm();
+        $reglasValidacion = $this->crearReglasDeValidacion();
+                                                                                                                                                                                                                                                                                                                                                                
+    }
+
+    /*private function mostrarFormulario()
     {
      //se muestra la vista del formulario (la plantilla form_bienvenida.php)   
         include 'vistas/form_bienvenida.php';
-    }
-    private function mostrarResultado($resultado)
+    }*/
+    private function mostrarFormulario($resultado)
     {
-    // y se muestra la vista del resultado (la plantilla resultado.,php)
+    // y se muestra la vista del resultado (la plantilla resultado.php)
         include 'vistas/form_bienvenida.php';
     }
 }
