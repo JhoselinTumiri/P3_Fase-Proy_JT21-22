b58d80c63d5ad5a23cd32857b68a8c30be4d0035
JhoselinTumiri <74874391+JhoselinTumiri@users.noreply.github.com>
2022-01-25 10:00:58 +0100

    "función validar Dni"

--- includes/funciones.php
+++ includes/funciones.php
@@ -6,4 +6,18 @@
         print_r($array);
         echo "</pre>";
     }
+
+    // Validar dni con php
+    function validarDni($dni){
+        $letra = substr($dni, -1);
+        $numeros = substr($dni, 0, -1);
+        $valido =true;
+        if (substr("TRWAGMYFPDXBNJZSQVHLCKE", $numeros % 23, 1) == $letra && strlen($letra) == 1 && strlen ($numeros) == 8 ){
+          $valido=true;
+        }else{
+          $valido=false;
+        }
+        return $valido;
+      }
+
 ?>
